f0c01176e7dd48c7ab0c02d9a0727ef8ed1832d1
LuK1337 <priv.luk@gmail.com>
2023-12-06 09:54:05 +0100

    gs-common: Add liboemcrypto.so to vendor linker config
    This lets com.google.android.widevine.apex access it.
    
    Change-Id: Ic33044bae63fc77700f4f3e6401e1363c9be3296

--- widevine/widevine.mk
+++ widevine/widevine.mk
@@ -1,4 +1,7 @@
 PRODUCT_PACKAGES += \
 	android.hardware.drm-service.clearkey \
 
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    device/google/gs-common/widevine/linker.config.json
+
 -include vendor/widevine/libwvdrmengine/apex/device/device.mk
